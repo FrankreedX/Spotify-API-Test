536a4b2736e1c53147bdbfc4305e189814f2f481
Frankreed <baoanhtdn@gmail.com>
2021-10-21 15:31:01 -0700

    post request to spotify to search for a user works

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -6,6 +6,27 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
  */
+
+function httpPost(url, res) {
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            // use the access token to access the Spotify Web API
+            var token = body.access_token;
+            var options = {
+                url: url,
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            };
+            request.get(options, function (error, response, bod) {
+                res.send(JSON.stringify(bod));
+            });
+        }
+    });
+}
+
 var express = require('express'); // Express web server framework
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -17,42 +38,23 @@ var client_secret = '4902813f323d40f48da01f88fed2ff17'; // Your secret
 
 // your application requests authorization
 var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
 };
 
-app.get('/connor',(req,res) => {
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
+app.post('/search/:q', (req, res) => {
+    httpPost('https://api.spotify.com/v1/search?q=' + req.params.q + '&type=artist&limit=1', res)
+})
 
-      // use the access token to access the Spotify Web API
-      var token = body.access_token;
-      var options = {
-        url: 'https://api.spotify.com/v1/users/12168028487',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      };
-      request.get(options, function(error, response, bod) {
-        // console.log(bod);
-        // res.write(JSON.stringify(bod));
-        res.send(JSON.stringify(bod));
-      });
-    }
-  });
-});
-
-app.get('/test', (req, res)=>{
-  res.send("heloo")
+app.get('/connor', (req, res) => {
+    httpPost('https://api.spotify.com/v1/users/12168028487', res);
 });
 
 console.log('Listening on 3777');
 app.listen(3777);
-
